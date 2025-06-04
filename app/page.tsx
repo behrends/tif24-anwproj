b4cc169afd7fd90cc8b4ab4862f4e5139104f3b7
Erik Behrends <behrends@users.noreply.github.com>
2025-06-04 09:27:14 +0200

    Text in Blau und Hinweis

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,10 @@ export default function HomePage() {
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-2xl font-bold">Home Page</h1>
-      <p className="text-xl font-extrabold text-red-500 mt-4">
+      <p className="text-xl font-extrabold text-blue-500 mt-4">
         Welcome to the home page!!!
       </p>
+      <p>Achtung: Beta-Version</p>
     </div>
   );
 }
